e994397a55d23e52ba4e6bc1c27d9b6c79b483dd
Peter Upfold <pgithub@upfold.org.uk>
2020-02-12 19:34:08 +0000

    ncurses menu

--- swtd.c
+++ swtd.c
@@ -3,6 +3,9 @@
 #include <unistd.h>
 #include <assert.h>
 
+#include <curses.h>
+#include <menu.h>
+
 typedef struct swtodo {
 	int flags;
 	char * title;
@@ -12,7 +15,31 @@ typedef struct swtodo {
 int main(int argc, char * argv[]) {
 
 
-	swtodo_t *mytodos = (swtodo_t*)malloc(sizeof(swtodo_t *) * argc);
+	ITEM **menu_items;
+	MENU *swtd_menu;
+
+	initscr();
+	cbreak();
+
+	noecho();
+
+	keypad(stdscr, TRUE);
+
+	menu_items = (ITEM **)calloc(2 /* size of plus one null item */, sizeof(ITEM *));
+
+	menu_items[0] = new_item("New", "New");
+	menu_items[1] = (ITEM *)NULL;
+
+	swtd_menu = new_menu((ITEM **)menu_items);
+	post_menu(swtd_menu);
+	refresh();
+
+	free_item(menu_items[0]);
+	free_item(menu_items[1]);
+	free_menu(swtd_menu);
+	endwin();
+
+	/*swtodo_t *mytodos = (swtodo_t*)malloc(sizeof(swtodo_t *) * argc);
 	assert(mytodos != NULL);
 
 	for (int i = 0; i < argc; i++) {
@@ -30,7 +57,7 @@ int main(int argc, char * argv[]) {
 
 	for (int i = 0; i < argc; i++) {
 		printf("mytodo %d is %s\n", i, mytodos[i].title);
-	}
+	}*/
 
 	return 0;
 }
