a22703b0de9936de54cc53fe8d46b58bfba15a4c
PeterUpfold <pgithub@upfold.org.uk>
2020-04-25 18:30:14 +0100

    Working menu with Enter

--- swtd.c
+++ swtd.c
@@ -6,6 +6,9 @@
 #include <curses.h>
 #include <menu.h>
 
+
+#include "swtd_strings.h"
+
 typedef struct swtodo {
 	int flags;
 	char * title;
@@ -15,11 +18,13 @@ typedef struct swtodo {
 int main(int argc, char * argv[]) {
 
 
-	ITEM **menu_items;
+	ITEM **our_menu_items;
 	MENU *swtd_menu;
 	int current_char = -1;
+	ITEM **items;
 
 	initscr();
+	start_color();
 	cbreak();
 
 	noecho();
@@ -27,15 +32,15 @@ int main(int argc, char * argv[]) {
 	keypad(stdscr, TRUE);
 	init_pair(1, COLOR_RED, COLOR_BLACK);
 
-	menu_items = (ITEM **)calloc(3 /* size of plus one null item */, sizeof(ITEM *));
+	our_menu_items = (ITEM **)calloc(3 /* size of plus one null item */, sizeof(ITEM *));
 
-	menu_items[0] = new_item("New", "New");
-	menu_items[1] = new_item("Edit", "Edit");
-	menu_items[2] = (ITEM *)NULL;
+	our_menu_items[0] = new_item(SWTD_NEW, SWTD_NEW);
+	our_menu_items[1] = new_item(SWTD_EDIT, SWTD_EDIT);
+	our_menu_items[2] = (ITEM *)NULL;
 
-	swtd_menu = new_menu((ITEM **)menu_items);
+	swtd_menu = new_menu((ITEM **)our_menu_items);
 
-	mvprintw(LINES - 2, 0, "F1 to exit");
+	mvprintw(LINES - 2, 0, SWTD_F1_TO_EXIT);
 
 	post_menu(swtd_menu);
 	refresh();
@@ -49,12 +54,31 @@ int main(int argc, char * argv[]) {
 			case KEY_UP:
 			menu_driver(swtd_menu, REQ_UP_ITEM);
 			break;
+			case 10: /* Enter */
+
+			items = menu_items(swtd_menu);
+
+			for (int i = 0; i < item_count(swtd_menu); i++) {
+				//printf("loop over %s\n", item_name(items[i]));
+
+				if (current_item(swtd_menu) == items[i]) {
+					if (strcmp(item_name(items[i]), SWTD_NEW) == 0) {
+						// handle new item
+
+					}
+					else if (strcmp(item_name(items[i]), SWTD_EDIT) == 0) {
+						// handle edit
+						
+					}
+				}
+			}
+			break;
 		}
 	}
 
-	free_item(menu_items[0]);
-	free_item(menu_items[1]);
-	free_item(menu_items[2]);
+	free_item(our_menu_items[0]);
+	free_item(our_menu_items[1]);
+	free_item(our_menu_items[2]);
 	free_menu(swtd_menu);
 	endwin();
 
---
+++ swtd_strings.h
@@ -0,0 +1,3 @@
+#define SWTD_NEW "New"
+#define SWTD_EDIT "Edit"
+#define SWTD_F1_TO_EXIT "F1 to exit"
